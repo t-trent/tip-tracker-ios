dcd7bb623b539552b0bae4b92d941fcc9bd74d9b
tccool <trent_carlson@icloud.com>
2025-02-27 11:02:38 -0800

    Fix text scaling issue

--- Tip Tracker/SettingsView.swift
+++ Tip Tracker/SettingsView.swift
@@ -69,6 +69,7 @@ struct SettingsView: View {
             .navigationTitle("Settings")
             .navigationBarTitleDisplayMode(.large)
         }
+        .dynamicTypeSize(.xSmall ... .large)
     }
 }
 
